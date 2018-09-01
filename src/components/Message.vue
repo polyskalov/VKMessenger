9713e08babdd2def41a7cb50e63a022235e4208b
Andrey Polyskalov <a@polyskalov.com>
2018-09-01 23:55:49 +0500

    Add line breaks to messages

--- src/components/Message.vue
+++ src/components/Message.vue
@@ -17,7 +17,12 @@ export default {
     },
     components: {
         Attachment
-    }
+    },
+    filters: {
+        nl2br: function(str, isXhtml) {
+            return String(str).replace(/([^>\r\n]?)(\r\n|\n\r|\r|\n)/g, '$1<br>$2');
+        }
+    }	
 }
 </script>
 
@@ -34,7 +39,7 @@ export default {
                 </a>
                 <span class="im-history__message__time">{{message.date | moment().format('HH:mm')}}</span>
             </div>
-            <div class="im-history__message__text" v-html="$options.filters.linkify(message.text)"></div>
+            <div class="im-history__message__text" v-html="$options.filters.nl2br($options.filters.linkify(message.text))"></div>
             <template v-if="message.fwd_messages">
                 <div v-for="fwd_message in message.fwd_messages" v-bind:key="fwd_message.date">
                     <strong>Forwarded message from id{{fwd_message.from_id}}:</strong> {{fwd_message.text}}
@@ -48,64 +53,64 @@ export default {
 
 <style>
 .im-history__day {
-	color: #828282;
-	text-align: center;
-	font-size: 12.5px;
-	background: #fff;
-	font-weight: 400;
-	position: relative;
-	margin: 10px 43px;
-	border: none;
-	padding: 0;
+    color: #828282;
+    text-align: center;
+    font-size: 12.5px;
+    background: #fff;
+    font-weight: 400;
+    position: relative;
+    margin: 10px 43px;
+    border: none;
+    padding: 0;
 }
 
 .im-history__message {
-	display: flex;
-	flex-shrink: 0;
-	font-size: 13px;
-	border-radius: 3px;
-	margin: 0 10px;
-	overflow: hidden;
-	padding: 5px 20px;
+    display: flex;
+    flex-shrink: 0;
+    font-size: 13px;
+    border-radius: 3px;
+    margin: 0 10px;
+    overflow: hidden;
+    padding: 5px 20px;
 }
 .im-history__message_unread {
-	background: #edf0f5;
+    background: #edf0f5;
 }
 
 .im-history__message__body {
-	overflow: hidden;
-	margin: 0 10px;
-	flex-grow: 1;
-	flex-shrink: 1;
+    overflow: hidden;
+    margin: 0 10px;
+    flex-grow: 1;
+    flex-shrink: 1;
 }
 .im-history__message__photo {
-	background-color: #fafbfc;
-	flex-shrink: 0;
-	height: 36px;
-	width: 36px;
-	margin: 5px;
-	margin-left: 15px;
+    background-color: #fafbfc;
+    flex-shrink: 0;
+    height: 36px;
+    width: 36px;
+    margin: 5px;
+    margin-left: 15px;
 }
 .im-history__message__meta {
-	font-size: 12px;
-	margin-top: 10px;
+    font-size: 12px;
+    margin-top: 10px;
 }
 .im-history__message__text {
-	margin-top: 5px;
-	word-wrap: break-word;
+    margin-top: 5px;
+    word-wrap: break-word;
 }
 .im-history__message__username {
-	max-width: 200px;
-	font-weight: bold;
-	white-space: nowrap;
-	overflow: hidden;
-	text-overflow: ellipsis;
-	display: inline-block;
-	vertical-align: top;
-	margin-right: 10px;
+    max-width: 200px;
+    font-weight: bold;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    display: inline-block;
+    vertical-align: top;
+    margin-right: 10px;
 }
 .im-history__message__time {
-	font-weight: 400;
-	color: rgba(120, 127, 140, 0.6);
+    font-weight: 400;
+    color: rgba(120, 127, 140, 0.6);
 }
 </style>
